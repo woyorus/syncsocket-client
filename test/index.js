ce602cbaf44c15ba299b4696f038a7d53559ad0b
Kirill Kravinsky <woyorus@gmail.com>
2016-08-10 14:28:27 -0400

    add connection-error event to connection object

--- test/index.js
+++ test/index.js
@@ -34,6 +34,11 @@ describe('connection', function () {
         });
     });
 
+    it('should not connect to server which doesn\'t exist', function (done) {
+        var conn = connect('http://dfaesf234rfgy234f23f23f.ca');
+        conn.on('connection-error', () => done());
+    });
+
     describe('joinChannel', function () {
         it('should join an existing channel', function (done) {
             var conn = connect(env.url);
